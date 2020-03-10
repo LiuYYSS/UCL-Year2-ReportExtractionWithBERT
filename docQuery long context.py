a9ca7a334d14f339d540b4c95d00b630a8b3737a
Yansong Liu <38743168+LiuYYSS@users.noreply.github.com>
2020-03-10 10:18:40 +0000

    .

--- docQuery long context.py
+++ docQuery long context.py
@@ -6,64 +6,94 @@ from simpletransformers.question_answering import QuestionAnsweringModel
 import platform
 import requests
 import pdf2image as pdf2image
+import docQuery_utils
 
-
-def to_list(tensor):
-    return tensor.detach().cpu().tolist()
-
-
+# ms cognitive service configs
 endpoint = 'https://uksouth.api.cognitive.microsoft.com/'
 ocr_url = 'https://uksouth.api.cognitive.microsoft.com/vision/v2.1/ocr'
 subscription_key = 'ffcc4bbd174c4b6e97d0a945aebf8b98'
 
-inputPath = input("PDF Path:")
-print("PDF path:", inputPath)
+# transformer configs
+model = QuestionAnsweringModel('albert', 'ahotrod/albert_xxlargev1_squad2_512',
+                               args={'max_seq_length': 512, "eval_batch_size": 3, "version_2_with_negative": True,
+                                     'reprocess_input_data': True, 'overwrite_output_dir': True, 'silent': True})
 
-print("starts converting PDF to image, please wait")
-pages = None
-if os.path.isfile(inputPath):
-    if platform.system() == 'Windows':
-        pathname = os.path.dirname(sys.argv[0])
-        pathname = os.path.abspath(pathname) + os.path.sep + "poppler_win" + os.path.sep + "bin"
-        pages = pdf2image.convert_from_path(inputPath, 200, poppler_path=pathname, fmt="jpeg")
+# input dir configs
+projectPath = os.path.abspath(os.path.dirname(sys.argv[0]))
+inputPath = projectPath + os.path.sep + "input"
+
+#program configs
+nBestProbability = 0.1
+
+pdfs = []
+for filename in os.listdir(inputPath):
+    if filename.endswith(".pdf"):
+        pdfs.append(os.path.join(inputPath, filename))
     else:
-        pages = pdf2image.convert_from_path(inputPath, 200, fmt="jpeg")
-else:
-    print("input file doesn't exist")
-    exit()
+        continue
 
+for pdf in pdfs:
+    pages = None
+    outputString = ""
+    pageNum = 0
 
-outputString = ""
-pageNum = 0
-while pageNum < 18:
-    print(pageNum)
-    imgByteArr = io.BytesIO()
-    pages[pageNum].save(imgByteArr, format=pages[pageNum].format)
-    imgByteArr = imgByteArr.getvalue()
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
-    params = {'language': 'unk', 'detectOrientation': 'true'}
-    response = requests.post(ocr_url, headers=headers, params=params, data=imgByteArr)
-    response.raise_for_status()
-    ocrResult = response.json()
-    ocrResult = json.dumps(eval(str(ocrResult)))
+    if platform.system() == 'Windows':
+        pathname = projectPath + os.path.sep + "poppler_win" + os.path.sep + "bin"
+        pages = pdf2image.convert_from_path(pdf, 200, poppler_path=pathname, fmt="jpeg")
+    else:
+        pages = pdf2image.convert_from_path(pdf, 200, fmt="jpeg")
 
-    resultJSONObject = json.loads(ocrResult)
-    regions = resultJSONObject['regions']
-    for region in regions:
-        lines = region['lines']
+    while pageNum < len(pages):
+        # pic to json
+        imgByteArr = io.BytesIO()
+        pages[pageNum].save(imgByteArr, format=pages[pageNum].format)
+        imgByteArr = imgByteArr.getvalue()
+        headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+        params = {'language': 'unk', 'detectOrientation': 'true'}
+        response = requests.post(ocr_url, headers=headers, params=params, data=imgByteArr)
+        response.raise_for_status()
+        ocrResult = response.json()
+        ocrResult = json.dumps(eval(str(ocrResult)))
 
-        for line in lines:
-            words = line['words']
-            for word in words:
-                text = word.get('text')
-                outputString += text + " "
-    pageNum +=1
-print(outputString)
+        # json to string
+        resultJSONObject = json.loads(ocrResult)
+        regions = resultJSONObject['regions']
+        for region in regions:
+            lines = region['lines']
 
-while(True):
+            for line in lines:
+                words = line['words']
+                for word in words:
+                    text = word.get('text')
+                    outputString += text + " "
+        pageNum += 1
+
+    # Questions!
     question = input("question:")
-    to_predict = [{'context': outputString, 'qas': [{'question': question, 'id': '0'}]}]
+    to_predict = [
+        {
+            'context': outputString,
+            'qas': [
+                {
+                    'id': "1",
+                    'is_impossible': False,
+                    'question': question,
+                    'answers': []
+                }
+            ]
+        }
+    ]
+    os.makedirs('data', exist_ok=True)
+    with open('data/train.json', 'w') as f:
+        json.dump(to_predict, f)
+    f.close()
+    allPrediction, nBestPrediction = model.eval_model('data/train.json')
+    print(allPrediction)
+    print(nBestPrediction)
 
-    model = QuestionAnsweringModel('albert', 'ahotrod/albert_xxlargev1_squad2_512',
-                                    args={'max_seq_length': 512, "eval_batch_size": 3, "version_2_with_negative": True, 'reprocess_input_data': True, 'overwrite_output_dir': True})
-    print(model.predict(to_predict)[0]['answer'])
+    # Extract answer
+    ansText = []
+    for Pred in nBestPrediction['1']:
+        if Pred['probability'] >= nBestProbability:
+            ansText.append(Pred['text'])
+    ansText = docQuery_utils.removeIncludePredictions(ansText)
--- docQuery.py
+++ docQuery.py
@@ -47,6 +47,7 @@ while pageNum < len(pages):
         pageNum += 1
     if makeQues.upper() == 'T':
         if prevNum != pageNum:
+            outputString = ""
             imgByteArr = io.BytesIO()
             pages[pageNum].save(imgByteArr, format=pages[pageNum].format)
             imgByteArr = imgByteArr.getvalue()
@@ -71,8 +72,9 @@ while pageNum < len(pages):
 
         question = input("question:")
         to_predict = [{'context': outputString, 'qas': [{'question': question, 'id': '0'}]}]
+        print(to_predict)
 
-        model = QuestionAnsweringModel('albert', 'ahotrod/albert_xxlargev1_squad2_512',
-                                       args={'max_seq_length': 512, "eval_batch_size": 3, "version_2_with_negative": True, 'reprocess_input_data': True, 'overwrite_output_dir': True, 'silent': True, "n_best_size":10})
-        res = model.predict(to_predict)
+        #model = QuestionAnsweringModel('albert', 'ahotrod/albert_xxlargev1_squad2_512', args={'max_seq_length': 512, "eval_batch_size": 3, "version_2_with_negative": True, 'reprocess_input_data': True, 'overwrite_output_dir': True, 'silent': True})
+        res = model.predict(to_predict, 10)
+        print(len(res))
         print(res[0]['answer'])
