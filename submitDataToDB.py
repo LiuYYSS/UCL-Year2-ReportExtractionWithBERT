4e123e76aade28da9be09234d5107cc49a407036
markansn <manson10@hotmail.co.uk>
2020-04-02 21:25:00 +0100

    final

--- submitDataToDB.py
+++ submitDataToDB.py
@@ -12,10 +12,10 @@ class submitDataToDB:
 
 
 	def create_connection(self):
-		self.connection = pymysql.connect(host='ancssc-db.mysql.database.azure.com',
-									 user='ancssc@ancssc-db',
-									 password='819UiC@Uj&$Z^GY',
-									 db='team_36_db_do_not_touch',
+		self.connection = pymysql.connect(host='',
+									 user='',
+									 password='',
+									 db='',
 									 charset='utf8mb4',
 									 cursorclass=pymysql.cursors.DictCursor)
 
